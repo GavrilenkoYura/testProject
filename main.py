e7dbdb82141a7792f34cf19c8fa31aa9d621fe89
yura <gshshaaha@gmail.com>
2023-11-20 01:44:26 +0300

    docs: change cell values in the table to title case

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ with open('data.csv') as file:
     book = openpyxl.Workbook()
     sheet = book.active
 
-    sheet['A2'] = 'id'
-    sheet.cell(row=3, column=1).value = 'name'
-    sheet[4][0].value = 'phone'
+    sheet['A2'] = 'Id'
+    sheet.cell(row=3, column=1).value = 'Name'
+    sheet[4][0].value = 'Phone'
 
-    column_labels = [f"person {i}" for i in range(1, len(lst_reader) + 1)]
+    column_labels = [f"Person {i}" for i in range(1, len(lst_reader) + 1)]
     for col, label in zip(range(2, len(lst_reader) + 2), column_labels):
         sheet.cell(row=1, column=col).value = label
 
     for idx, row in enumerate(lst_reader, start=2):
-        sheet.cell(row=2, column=idx).value = row['id']
-        sheet.cell(row=3, column=idx).value = row['name']
-        sheet.cell(row=4, column=idx).value = row['phone']
+        sheet.cell(row=2, column=idx).value = row['Id']
+        sheet.cell(row=3, column=idx).value = row['Name']
+        sheet.cell(row=4, column=idx).value = row['Phone']
 
     book.save('data.xlsx')
     book.close()
